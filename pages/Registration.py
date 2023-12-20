af4a77394d36b80aa2fcce2106221837887ef776
MYGolubev <pretorianeclegiona@gmail.com>
2023-12-20 17:47:37 +0300

    Hometask10

--- pages/Registration.py
+++ pages/Registration.py
@@ -1,62 +1,44 @@
 from selene.support.conditions import have, be
 from selene.support.shared import browser
 from utils.load_file import path
-
-import random
+from users.user import User
 
 
 class Registration:
 
-    def set_first_name(self, first_name):
-        browser.element("#firstName").type(first_name)
-
-    def set_last_name(self, last_name):
-        browser.element("#lastName").type(last_name)
-
-    def set_email(self, email):
-        browser.element("#userEmail").type(email)
-
-    def set_mobile(self, mobile):
-        browser.element("#userNumber").type(mobile)
-
-    def set_current_address(self, address):
-        browser.element("#currentAddress").type(address)
-
-    def set_subject(self, subject):
-        browser.element("#subjectsInput").type(subject).press_enter()
-
-    def select_gender(self):
-        random_gender = str(random.randint(1, 3))
-        gender_dict = {"1": "Male", "2": "Female", "3": "Other"}
-        browser.element('[for="gender-radio-' + random_gender + '"]').click()
-        return gender_dict[random_gender]
-
-    def select_hobbies(self):
-        random_hobbies = str(random.randint(1, 3))
-        hobbies_dict = {"1": "Sports", "2": "Reading", "3": "Music"}
-        browser.element('[for="hobbies-checkbox-' + random_hobbies + '"]').click()
-        return hobbies_dict[random_hobbies]
-
-    def select_state(self, state):
-        browser.element("#state").click().all('[id^="react-select-3-option"]').element_by(have.exact_text(state)).click()
-
-    def select_city(self, city):
-        browser.element("#city").click().all('[id^="react-select-4-option"]').element_by(have.exact_text(city)).click()
-
-    def set_birthday(self, day, month, year):
+    def register_user(self, user: User):
+        browser.element("#firstName").type(user.first_name)
+        browser.element("#lastName").type(user.last_name)
+        browser.element("#userEmail").type(user.email)
+        browser.element("#userNumber").type(user.mobile)
+        browser.element("#currentAddress").type(user.address)
+        browser.element("#subjectsInput").type(user.subject).press_enter()
+        browser.all('input[name=gender]').element_by(have.value(user.gender)).element('..').click()
+        browser.all('.custom-checkbox').element_by(have.exact_text(user.hobby)).click()
         browser.element('#dateOfBirthInput').should(be.not_.blank).click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
-        browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click()
+        browser.element('.react-datepicker__month-select').type(user.date_of_birth.strftime('%B'))
+        browser.element('.react-datepicker__year-select').type(user.date_of_birth.year)
+        browser.element(
+            f'.react-datepicker__day--0{user.date_of_birth.day}:not(.react-datepicker__day--outside-month)').click()
+        browser.element("#uploadPicture").send_keys(path(user.picture))
+        browser.element('#state').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(user.state)).click()
+        browser.element('#city').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(user.city)).click()
+        browser.element('#submit').press_enter()
 
-    def upload_picture(self, picture):
-        browser.element("#uploadPicture").send_keys(path(picture))
-
-    def submit(self):
-        browser.element("#userNumber").press_enter()
-
-    def assert_registration_data(self, first_name, last_name, email, mobile, address, subject, gender, hobbie, state,
-                                 city, day, month, year, picture):
-        browser.element('.table-responsive').all('td:nth-child(2)').should(
-            have.texts(f"{first_name} {last_name}", email, gender, mobile, f"{day} {month},{year}",
-                       subject, hobbie, picture, address, f"{state} {city}"))
+    def assert_registration_data(self, user: User):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                f'{user.first_name} {user.last_name}',
+                user.email,
+                user.gender,
+                user.mobile,
+                user.date_of_birth.strftime('%d %B,%Y'),
+                user.subject,
+                user.hobby,
+                user.picture,
+                user.address,
+                f'{user.state} {user.city}',
+            )
+        )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+import datetime
 from selene import browser
+from users.user import User
 
 
 @pytest.fixture(autouse=True)
@@ -8,3 +10,21 @@ def browser_launch():
     browser.open('https://demoqa.com/automation-practice-form')
     yield
     browser.close()
+
+
+@pytest.fixture()
+def create_user():
+    return User(
+        first_name='RandomName',
+        last_name='RandomFamilyName',
+        email='user@qa.com',
+        gender='Male',
+        mobile='9991111111',
+        date_of_birth=datetime.date(1993, 9, 24),
+        subject='Arts',
+        hobby='Music',
+        picture='Duck.jpg',
+        address='Street',
+        state='Uttar Pradesh',
+        city='Lucknow'
+    )
